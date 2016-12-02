af55ca9c239f44d810bb542abe72af936fac5930
James Kozianski <soulplant@gmail.com>
2016-12-02 20:27:17 +1100

    Add unstaged changes check to deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ `expr $(git status --porcelain --untracked-files=no | wc -l)` != 0 ]; then
+  echo "You have unstaged changes, cannot deploy."
+  exit 1
+fi
+
 git branch -D deploy
 git checkout -b deploy
 gradle ngBuild
