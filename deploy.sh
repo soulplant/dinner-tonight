fe4b4a583c98f718b85b1c1f391db4dd66607013
James Kozianski <soulplant@gmail.com>
2016-12-05 22:19:30 +1100

    Generate build.gradle for heroku.

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,10 @@ git checkout -b deploy
 bazel build java/bookr:bookr_deploy.jar
 mkdir -p bin
 cp bazel-bin/java/bookr/bookr{,_deploy.jar} bin
+echo 'task stage()' > build.gradle
 git add -f client/dist
 git add -f bin
+git add -f build.gradle
 git commit -m "Deploy @ `date`"
 git push heroku deploy:master --force
 git checkout master
